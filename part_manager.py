2b167eb844a7fbab2f3ef7fcf9d7616e2f5b8cf8
Kipkorir74 <49982743+Kipkorir74@users.noreply.github.com>
2023-02-14 10:58:04 +0300

    Validating items

--- part_manager.py
+++ part_manager.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+from tkinter import messagebox
 from db import Database
 
 db = Database('store.db')
@@ -10,6 +11,10 @@ def populate_list():
         parts_list.insert(END, row)
 
 def add_item():
+    if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '' :
+        messagebox.showerror('Required Fields', 'Please Enter all the Required Fields')
+        return
+        
     print(db.insert(part_text.get(), customer_text.get(), retailer_text.get(), price_text.get()))
     parts_list.delete(0, END)
     parts_list.insert(END, (part_text.get(), customer_text.get(), retailer_text.get(), price_text.get()))
